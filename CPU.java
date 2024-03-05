0fea97b8503148c0083280482bca743a386367cb
Poojamanojmm <145551303+Poojamanojmm@users.noreply.github.com>
2024-03-05 15:36:17 +0530

    Add files via upload

--- CPU.java
+++ CPU.java
@@ -1,43 +1,55 @@
 public class CPU 
-{ int price;
-class Processor 
-{ 
+{
+int price;
+CPU (int price)
+{
+this.price=price;
+}
+public void display()
+{
+System.out.println("\nCPU info");
+System.out.println("CPU price is "+price);
+}
+class Processor /* nested class named "Processor" inside the CPU class. This nested class
+will contain properties and behaviors related to the processor of the CPU. */
+{
 int cores;
 String producer;
-Processor (int noC, String Anu)
+Processor(int cores, String producer)
 {
-cores = noC;
-producer = Anu;
+this.cores=cores;
+this.producer=producer;
 }
 void display()
 {
-System.out.println ("\nProcessor info");
-System.out.println ("No. of Cores = "+cores);
-System.out.println ("Manufacturer = "+producer+"\n");
+System.out.println("\nProcessor info");
+System.out.println("No. of Cores = "+cores);
+System.out.println("Manufacturer = "+producer);
 }
 }
-static class RAM 
+static class RAM /* nested static class named "RAM" inside the CPU class. This nested class will
+contain properties & behaviors related to the RAM of the CPU.*/
 {
-int mem;
-String manuf;
-RAM (int memory, String producer)
+int memory;
+String producer;
+RAM (int memory, String producer )
 {
-mem = memory;
-manuf = producer;
+this.memory=memory;
+this.producer=producer;
 }
-void display ( )
+void display()
 {
-System.out.println ("\n RAM info");
-System.out.println ("Memory = "+mem+" GB");
-System.out.println ("Manufacturer = "+manuf+"\n");
+System.out.println("\nRAM info");
+System.out.println("Memory = "+memory+" GB");
+System.out.println("Manufacturer = "+producer);
 }
-}
-public static void main(String[] args)
+}public static void main(String[] args)
 {
-CPU.RAM obj1= new CPU.RAM (8,"Intel");
-CPU obj2 = new CPU ( );
-CPU.Processor obj3 = obj2.new Processor (8,"Samsung");
-obj1. display( );
-obj3. display( );
+CPU cpuobj = new CPU(30000);
+CPU.Processor probj = cpuobj.new Processor (8,"Samsung"); 
+CPU.RAM ramobj= new CPU.RAM(8,"Intel");
+cpuobj.display();
+probj.display();
+ramobj.display();
 }
 }
